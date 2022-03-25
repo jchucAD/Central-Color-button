c1914c4282181a79cb33c76cb26026ea42b1ef9f
Jean-Christophe Huc <85747030+jchucAD@users.noreply.github.com>
2022-03-25 14:17:20 +0100

    rajout test 2.1

--- src/App.test.js
+++ src/App.test.js
@@ -6,7 +6,7 @@ test('Nom test 1 : renders learn react link', () => {
   expect(screen.getByText(/learn react/i)).toBeInTheDocument()
 })
 
-test('Mon test 2q: full app rendering/navigating', () => {
+test('Mon test 2: full app rendering/navigating', () => {
   render(<App />)
   expect(screen.getByText(/save/i)).toBeInTheDocument()
   // userEvent.click(screen.getByText(/about/i))
